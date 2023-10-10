f9d50f1cc819e7665ff57fffbf0a2edfceb77e63
jawwad-farooq <jawwad.farooq@eusopht.com>
2023-10-11 01:58:41 +0500

    adding password reset

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,49 @@ use App\Http\Controllers\PermissionController;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Middleware\Session;
 use App\Http\Middleware\checkAge;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+ 
+Route::post('/reset-password', function (Request $request) {
+    $request->validate([
+        'token' => 'required',
+        'email' => 'required|email',
+        'password' => 'required|min:8|confirmed',
+    ]);
+ 
+    $status = Password::reset(
+        $request->only('email', 'password', 'password_confirmation', 'token'),
+        function (User $user, string $password) {
+            $user->forceFill([
+                'password' => Hash::make($password)
+            ])->setRememberToken(Str::random(60));
+ 
+            $user->save();
+ 
+            event(new PasswordReset($user));
+        }
+    );
+ 
+    return $status === Password::PASSWORD_RESET
+                ? redirect()->route('login')->with('status', __($status))
+                : back()->withErrors(['email' => [__($status)]]);
+})->middleware('guest')->name('password.update');
+ 
+Route::post('/forgot-password', function (Request $request) {
+    $request->validate(['email' => 'required|email']);
+ 
+    $status = Password::sendResetLink(
+        $request->only('email')
+    );
+ 
+    return $status === Password::RESET_LINK_SENT
+                ? back()->with(['status' => __($status)])
+                : back()->withErrors(['email' => __($status)]);
+})->middleware('guest')->name('password.email');
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +62,14 @@ use App\Http\Middleware\checkAge;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+MAIL_MAILER=smtp
+MAIL_HOST=smtp.gmail.com
+MAIL_PORT=1025
+MAIL_USERNAME=jawadmuhamad786@gmail.com
+MAIL_PASSWORD=gvux cubh asgq omov
+MAIL_ENCRYPTION=ssl
+MAIL_FROM_ADDRESS=jawadmuhamad786@gmail.com
+MAIL_FROM_NAME="todo app"
 */
 
 
@@ -67,6 +118,13 @@ Route::group(['Middleware' => "web"], function () {
     Route::post('update-user/{id}', [UserController::class, 'Update']);
 });
 
+Route::get('/forgot-password', function () {
+    return view('auth.forgot-password');
+})->middleware('guest')->name('password.request');
+
+Route::get('/reset-password/{token}', function (string $token) {
+    return view('auth.reset-password', ['token' => $token]);
+})->middleware('guest')->name('password.reset');
 
 Route::get('ajax', function () {
     return view('message');
